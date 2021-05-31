24482e3d9944a7063a3112105dac41e8fc4e6acd
alexSmkh <alexshikari9333@gmail.com>
2021-05-31 21:48:52 +0300

    Added context for projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const USER_ID = process.env.REACT_APP_USER_ID;
+export const DEFAULT_SELECTED_PROJECT = 'INBOX';
 
 export const COLLATED_TASKS = [
   { key: 'INBOX', name: 'Inbox' },
---
+++ src/context/selected-project-context.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState } from 'react';
+import { DEFAULT_SELECTED_PROJECT } from '../constants/index';
+
+export const SelectedProjectContext = createContext();
+
+export const SelectedProjectProvider = ({ children }) => {
+  const { selectedProject, setSelectedProject } = useState(
+    DEFAULT_SELECTED_PROJECT,
+  );
+
+  return (
+    <SelectedProjectContext.Provider
+      value={{ selectedProject, setSelectedProject }}
+    >
+      {children}
+    </SelectedProjectContext.Provider>
+  );
+};
+
+export const useSelectedProjectValue = () => useContext(SelectedProjectContext);
