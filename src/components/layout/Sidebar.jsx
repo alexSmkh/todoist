4b223aaa1cb4267061bffacf2f2e3d53b6ef47f7
alexSmkh <alexshikari9333@gmail.com>
2021-06-02 00:44:21 +0300

    Fixed switching between main projects

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import {
@@ -7,16 +9,26 @@ import {
   FaCalendarDay,
 } from 'react-icons/fa';
 import { AddProject } from '../AddProject';
-
 import { Projects } from '../Projects';
+import { DEFAULT_SELECTED_PROJECT } from '../../constants';
+import { useSelectedProjectValue } from '../../context';
 
 export const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
+  const [active, setActive] = useState(DEFAULT_SELECTED_PROJECT);
+  const { setSelectedProject } = useSelectedProjectValue();
 
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li className="inbox" data-testid="inbox">
+        <li
+          className={active === 'inbox' ? 'active' : ''}
+          data-testid="inbox"
+          onClick={() => {
+            setActive('inbox');
+            setSelectedProject('INBOX');
+          }}
+        >
           <div>
             <span>
               <FaInbox />
@@ -24,7 +36,14 @@ export const Sidebar = () => {
             <span>Inbox</span>
           </div>
         </li>
-        <li className="today" data-testid="today">
+        <li
+          className={active === 'today' ? 'active' : ''}
+          data-testid="today"
+          onClick={() => {
+            setActive('today');
+            setSelectedProject('TODAY');
+          }}
+        >
           <div>
             <span>
               <FaCalendarDay />
@@ -32,7 +51,14 @@ export const Sidebar = () => {
             <span>Today</span>
           </div>
         </li>
-        <li className="next_7" data-testid="next_7">
+        <li
+          className={active === 'next_7' ? 'active' : ''}
+          data-testid="next_7"
+          onClick={() => {
+            setActive('next_7');
+            setSelectedProject('NEXT_7');
+          }}
+        >
           <div>
             <span>
               <FaCalendarWeek />
@@ -49,9 +75,7 @@ export const Sidebar = () => {
         <h2>Projects</h2>
       </div>
 
-      <ul className="sidebar__projects">
-        {showProjects && <Projects />}
-      </ul>
+      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
 
       {showProjects && <AddProject />}
     </div>
