a975594f53fe3dc0448481df143c7e843e23ed9a
alexSmkh <alexshikari9333@gmail.com>
2021-06-03 01:03:01 +0300

    Added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 import { ProjectsProvider, SelectedProjectProvider } from './context';
 
-export const App = () => (
-  <SelectedProjectProvider>
-    <ProjectsProvider>
-      <div className="App">
-        <Header />
-        <Content />
-      </div>
-    </ProjectsProvider>
-  </SelectedProjectProvider>
-);
+export const App = ({ darkModeDefault = false }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
+  return (
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <main className={darkMode ? 'darkmode' : ''} data-testid="application">
+          <Header darkmode={darkMode} setDarkMode={setDarkMode} />
+          <Content />
+        </main>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
+  );
+};
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
 
-export const Header = () => (
-  <header className="header" data-testid="header">
-    <nav>
-      <div className="logo">
-        <img src="/images/logo.png" alt="Todoist" />
-      </div>
-      <div className="settings">
-        <ul>
-          <li className="settings__add" data-testid="quick-add-task-action">+</li>
-          <li className="settings__darkmode" data-testid="dark-mode-action">
-            <FaPizzaSlice />
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-);
+export const Header = ({ darkmode, setDarkMode }) => {
+  const [shouldShowMain, setShouldMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todoist" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li className="settings__add" data-testid="quick-add-task-action">
+              +
+            </li>
+            <li
+              className="settings__darkmode"
+              data-testid="dark-mode-action"
+              onClick={() => setDarkMode(!darkmode)}
+            >
+              <FaPizzaSlice />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
